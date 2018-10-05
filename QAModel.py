f16944feb0d72281be7d84862451e3aaddd2db5c
Abdelrahman Nagy <abdelrahmannagy1@gmail.com>
2018-10-05 18:26:37 +0200

    top level qa model

---
+++ QAModel.py
@@ -0,0 +1,17 @@
+class QAModel:
+    """
+        Question Answering Module
+    """
+
+    def __init__(self, id2word, word2id, emb_matrix):
+        self.id2word = id2word
+        self.word2id = word2id
+
+
+    def add_placeholders(self):
+
+
+    def build_graph(self):
+
+
+    def add_loss(self):
